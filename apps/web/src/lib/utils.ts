67b8970e9f209ec0fe71c3abecae8e4a38c79d23
Wes Lowe <wesley.lowe@yahoo.com>
2024-03-22 13:56:07 -0500

    Delete unnecessary SVG files and add utility functions

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,5 +1,10 @@
 import { env } from "@/env";
+import { appConfig } from "@replyon/shared/lib/constants";
 
 export function buildAPIUrl(path: string) {
 	return env.NEXT_PUBLIC_API_URL + path;
+}
+
+export function buildTitle(pageName: string, appName: string = appConfig.name) {
+	return `${pageName} | ${appConfig.name}`;
 }
