e2cb863255b1dfb0dc3f94af21f928caf71ae1e3
Wes Lowe <wesley.lowe@yahoo.com>
2024-04-07 21:39:11 -0500

    updated Procfile

--- apps/server/src/controllers/stripe.ts
+++ apps/server/src/controllers/stripe.ts
@@ -1,8 +1,12 @@
-import { Controller, Post, Req } from "routing-controllers";
+import { Controller, Get, Post, Req } from "routing-controllers";
 import type { Request } from "express";
 
 @Controller("/stripe")
 export class StripeController {
-    @Post("/webhook")
-    async handleStripeWebhook(@Req() req: Request) {}
+	@Post("/webhook")
+	async handleStripeWebhook() {
+		console.log("stripe webhook received");
+
+		return "test";
+	}
 }
