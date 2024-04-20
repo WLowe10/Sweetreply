6d5035008d870a8ee4c015aa7171789ce878a8f1
Wes Lowe <wesley.lowe@yahoo.com>
2024-04-19 21:06:00 -0500

    fixed auth layout to add some margin on the base breakpoint

--- apps/web/src/routes/_auth+/_layout.tsx
+++ apps/web/src/routes/_auth+/_layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function AuthLayout() {
 	return (
-		<Box mt={{ base: 0, sm: "6rem" }}>
+		<Box mt={{ base: "lg", sm: "6rem" }}>
 			<Outlet />
 		</Box>
 	);
