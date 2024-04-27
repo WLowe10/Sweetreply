6ea35e57a9537ea4df427ebd2706329b1a1e0e84
Wes Lowe <wesley.lowe@yahoo.com>
2024-04-27 00:52:55 -0500

    made is such that reddit lead gen job can't overlap

--- apps/server/src/features/lead-engine/jobs/reddit-lead-gen/index.ts
+++ apps/server/src/features/lead-engine/jobs/reddit-lead-gen/index.ts
@@ -9,98 +9,100 @@ import { processLeadQueue } from "../../queues/process-lead";
 import { RedditCommentSlurper } from "./comment-slurper";
 
 const postSlurper = new RedditPostSlurper();
-// const commentSlurper = new RedditCommentSlurper();
+const commentSlurper = new RedditCommentSlurper();
 
-// const slurp = (slurper: RedditPostSlurper | RedditCommentSlurper) => {
-// 	try {}
-// };
+const executeSlurper = async (
+	slurper: RedditPostSlurper | RedditCommentSlurper,
+	projects: Project[]
+) => {
+	const leads = await slurper.slurp();
 
-const slurpPosts = async (projects: Project[]) => {
-	try {
-		const timeStart = Date.now();
-		const leads = await postSlurper.slurp();
+	for (const lead of leads) {
+		const searchDocument = postSlurper.getSearchDocument(lead);
+		const limit = pLimit(20);
 
-		for (const lead of leads) {
-			const searchDocument = postSlurper.getSearchDocument(lead);
-			const limit = pLimit(20);
+		await Promise.all(
+			projects.map((project) =>
+				limit(async () => {
+					if (!project.reddit_allow_nsfw && lead.is_nsfw) {
+						return;
+					}
 
-			await Promise.all(
-				projects.map((project) =>
-					limit(async () => {
-						if (!project.reddit_allow_nsfw && lead.is_nsfw) {
-							return;
-						}
+					if (
+						project.reddit_included_subreddits.length > 0 &&
+						!project.reddit_included_subreddits.includes(lead.channel)
+					) {
+						return;
+					}
 
-						if (
-							project.reddit_included_subreddits.length > 0 &&
-							!project.reddit_included_subreddits.includes(lead.channel)
-						) {
-							return;
-						}
+					if (
+						project.reddit_excluded_subreddits.length > 0 &&
+						project.reddit_excluded_subreddits.includes(lead.channel)
+					) {
+						return;
+					}
 
-						if (
-							project.reddit_excluded_subreddits.length > 0 &&
-							project.reddit_excluded_subreddits.includes(lead.channel)
-						) {
-							return;
-						}
+					let isMatch = false;
 
-						let isMatch = false;
+					try {
+						isMatch = test(parse(project.query as string), searchDocument);
+					} catch {
+						// noop
+					}
 
-						try {
-							isMatch = test(parse(project.query as string), searchDocument);
-						} catch {
-							// noop
-						}
+					if (!isMatch) {
+						return;
+					}
 
-						if (!isMatch) {
-							return;
-						}
+					const existingLead = await prisma.lead.findFirst({
+						where: {
+							project_id: project.id,
+							remote_id: lead.remote_id,
+						},
+					});
 
-						const existingLead = await prisma.lead.findFirst({
-							where: {
+					if (!existingLead) {
+						const newLead = await prisma.lead.create({
+							data: {
 								project_id: project.id,
+								type: lead.type,
+								title: lead.title,
+								content: lead.content,
+								date: lead.date,
+								platform: lead.platform,
+								username: lead.username,
+								channel: lead.channel,
 								remote_id: lead.remote_id,
+								remote_user_id: lead.remote_user_id,
+								remote_channel_id: lead.remote_channel_id,
+								remote_url: lead.remote_url,
 							},
 						});
 
-						if (!existingLead) {
-							const newLead = await prisma.lead.create({
-								data: {
-									project_id: project.id,
-									type: lead.type,
-									title: lead.title,
-									content: lead.content,
-									date: lead.date,
-									platform: lead.platform,
-									username: lead.username,
-									channel: lead.channel,
-									remote_id: lead.remote_id,
-									remote_user_id: lead.remote_user_id,
-									remote_channel_id: lead.remote_channel_id,
-									remote_url: lead.remote_url,
-								},
-							});
-
-							// processLeadQueue.add({
-							// 	lead_id: newLead.id,
-							// });
-						}
-					})
-				)
-			);
-		}
-
-		logger.info(`Slurped ${leads.length} reddit posts in ${Date.now() - timeStart}ms`);
-	} catch (err) {
-		logger.error(err, "Reddit post slurp failed");
+						processLeadQueue.add({
+							lead_id: newLead.id,
+						});
+					}
+				})
+			)
+		);
 	}
+
+	return leads;
 };
+
+let isRunning = false;
 
 // todo, job should not start if already running
 export const redditLeadGenJob = CronJob.from({
 	cronTime: "* * * * *",
-	onTick: async () => {
+	onTick: async function () {
+		if (isRunning) {
+			return;
+		}
+
+		isRunning = true;
+
 		try {
 			const projects = await prisma.project.findMany({
 				where: {
@@ -120,9 +122,39 @@ export const redditLeadGenJob = CronJob.from({
 				},
 			});
 
-			await Promise.allSettled([slurpPosts(projects)]);
+			const slurpPosts = async () => {
+				try {
+					const timeStart = Date.now();
+					const leads = await executeSlurper(postSlurper, projects);
+
+					logger.info(
+						`Slurped ${leads.length} reddit posts in ${Date.now() - timeStart}ms`
+					);
+				} catch (err) {
+					logger.error(err, "Reddit post slurp failed");
+				}
+			};
+
+			const slurpComments = async () => {
+				try {
+					const timeStart = Date.now();
+					const leads = await executeSlurper(commentSlurper, projects);
+
+					logger.info(
+						`Slurped ${leads.length} reddit comments in ${Date.now() - timeStart}ms`
+					);
+				} catch (err) {
+					logger.error(err, "Reddit comment slurp failed");
+				}
+			};
+
+			// comments slurping is disabled for now due to the possibility of an infinite loop of bot replies,
+			// since the bot replies will be picked up by the comment slurper
+			await Promise.allSettled([slurpPosts()]);
 		} catch (err) {
 			logger.error(err, "Reddit lead gen job failed");
 		}
+
+		isRunning = false;
 	},
 });
