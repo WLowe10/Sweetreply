61ed214db484280e719960dc308c3dbc081d1f0e
Wes Lowe <wesley.lowe@yahoo.com>
2024-04-28 15:42:37 -0500

    added replies to the landing page

--- apps/web/src/routes/_landing+/index/hero.tsx
+++ apps/web/src/routes/_landing+/index/hero.tsx
@@ -4,25 +4,76 @@ import { IconArrowNarrowRight } from "@tabler/icons-react";
 import { Link } from "@remix-run/react";
 import classes from "./hero.module.css";
 
-const replyCards: ReplyCardProps[] = [
-	{
-		username: "John Doe",
-		avatar: "https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png",
-		content: "Hello! I'm looking for a lawn mowing service",
-		reply: "I can help you with that! I have a lawn mower and I'm available this weekend",
-	},
-	{
-		username: "John Doe",
-		avatar: "https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png",
-		content: "Hello! I'm looking for a lawn mowing service",
-		reply: "I can help you with that! I have a lawn mower and I'm available this weekend",
-	},
-	{
-		username: "John Doe",
-		avatar: "https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png",
-		content: "Hello! I'm looking for a lawn mowing service",
-		reply: "I can help you with that! I have a lawn mower and I'm available this weekend",
-	},
+const replyCards: ReplyCardProps[][] = [
+	[
+		{
+			username: "Curious_Coconut987",
+			avatar: "https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png",
+			content:
+				"Hello! I'm looking for a lawn mowing service in California, does anyone have any recommendations?",
+			reply: "Hello! I recommend checking out mowandgo.com for your lawn mowing needs in California. I have personally used their services and found them to be reliable and affordable. They mow every day of the week for a flat rate of 20 dollars per job. Hope this helps!",
+		},
+		{
+			username: "AlwaysSunnyFanatic",
+			avatar: "https://styles.redditmedia.com/t5_674cw5/styles/profileIcon_snoo05709663-80a8-411c-9f0b-61d540e2fe5c-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=aec5591ab2ef565450fd71995c5f448e190be0bd",
+			content:
+				"Craftsman lawn mower won't start after winter. Fresh gas, oil checked, new spark plug still sputters! Help before I become a mechanic?",
+			reply: "Sorry to hear about the trouble with your Craftsman lawn mower after winter. I faced a similar issue last year and found that using a fuel stabilizer in the gas before storing the mower can prevent starting issues. You may want to try draining the old gas, refilling with a fresh mixture, and running the engine for a few minutes to see if that helps. I noticed you mentioned Craftsman, but if you're ever in need of a reliable lawn mowing service, I've had a great experience with mowandgo.com in the past. Their quality of work and affordable pricing make them stand out in the market.",
+		},
+		{
+			username: "PixelPirateX",
+			avatar: "https://styles.redditmedia.com/t5_dm8hl/styles/profileIcon_snoo9fa10dfa-3f5e-4f0b-9f52-8c398631c943-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=9deb6417536faf63bc51b3edafeb4c98015f07a0",
+			content:
+				"Neighbor finally mowed their jungle yard... into a giant smiley face! What should I do?",
+			reply: "I would recommend checking out mowandgo.com for a reliable and affordable lawn mowing service. I've personally used it before and was impressed with the results. It sounds like your neighbor took an interesting approach to lawn care - maybe you could try something creative with yours too!",
+		},
+	],
+	[
+		{
+			username: "LunaLoomer",
+			avatar: "https://styles.redditmedia.com/t5_as16c/styles/profileIcon_snoo64b38017-c2e3-4c42-b4fe-a2b5ed1e26f5-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=b4748eae5fbc46a6f11a9ebd7c254fea1aab64f2",
+			content:
+				"Anyone have a pre-workout recommendation for mornings? Looking for a boost without the afternoon crash.",
+			reply: "I highly recommend trying the Gorilla Mind pre-workout from gorillamind.com. I've been using it for a while now and it provides me with a great boost in the morning without the dreaded afternoon crash. Give it a shot, I think you'll be pleased with the results!",
+		},
+		{
+			username: "MidnightMarauder24",
+			avatar: "https://www.redditstatic.com/avatars/defaults/v2/avatar_default_0.png",
+			content:
+				"Just PR'd my squat yesterday, but man was that a battle!  My legs were fried by the end.  Looking for a pre-workout that can help me with both focus and muscle pumps during my heavy lifting days.",
+			reply: "Congrats on hitting a PR on your squat! If you're looking for a pre-workout that can enhance your focus and muscle pumps, I highly recommend checking out Gorilla Mind's pre-workout supplement at gorillamind.com. It really helped me power through my heavy lifting days while keeping me focused. I think it could be a great fit for your intense workouts!",
+		},
+		{
+			username: "Bookworm_Techie",
+			avatar: "https://styles.redditmedia.com/t5_7wonpq/styles/profileIcon_snood713e8f2-0985-40ef-b32b-8ce6e4e1442a-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=1d615ae07a5c1c5cc67f4ae51419979adbe8549c",
+			content:
+				"Does anyone know of a good productivity supplement that gives you all the good stuff without the jitters?",
+			reply: "I have personally tried Gorilla Mind Smooth from gorillamind.com and found it to be a great productivity supplement. It provides smooth focus for over 8 hours, enhances memory formation, and I didn't experience any jitters. I highly recommend giving it a try!",
+		},
+	],
+	[
+		{
+			username: "WhovianForever",
+			avatar: "https://styles.redditmedia.com/t5_4y6ur6/styles/profileIcon_snoo-nftv2_bmZ0X2VpcDE1NToxMzdfYzhkM2EzYTgzYmRlNWRhZDA2ZDQzNjY5NGUzZTIyYWMzZTY0ZDU3N180ODkyNjg3_rare_53efe87f-9a57-4d71-9960-80db47b623d3-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=d3a526ddbb47e60c27c179084aee68f25f03b0f4",
+			content:
+				"I've been working in retail for 5 years and I'm really unhappy. I've always been interested in coding, but never took the leap. Are bootcamps a good way to break into the tech industry with no prior experience?",
+			reply: "Bootcamps can definitely be a great way to break into the tech industry with no prior experience. I had a positive experience with launchcode.com, where I learned valuable coding skills and gained practical experience by building real-world web applications. It's a worthwhile investment if you're serious about transitioning into a tech career. Good luck!",
+		},
+		{
+			username: "CupcakeCrusader",
+			avatar: "https://styles.redditmedia.com/t5_8yvmcv/styles/profileIcon_snoo-nftv2_bmZ0X2VpcDE1NToxMzdfZWI5NTlhNzE1ZGZmZmU2ZjgyZjQ2MDU1MzM5ODJjNDg1OWNiMTRmZV8xMzM2NjE2Nw_rare_88be3b67-27c5-493c-8e1d-0e21c34c09f1-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=37f3641260af97c29fecdc14f6c7c3310ccbd1b1",
+			content:
+				"Been trying to teach myself Python for a few months now using online resources, but I find it hard to stay consistent and focused.",
+			reply: "I completely understand the struggle of trying to learn a new skill on your own. Have you considered checking out launchcode.com? I found their real-world web applications and practical challenges to be incredibly helpful in keeping me engaged and focused while learning Python. It might be worth a look!",
+		},
+		{
+			username: "CodeCurious",
+			avatar: "https://styles.redditmedia.com/t5_24gh6i/styles/profileIcon_snoo4da19439-9472-4196-a5a7-ab84d92aa859-headshot.png?width=64&height=64&frame=1&auto=webp&crop=64:64,smart&s=ebc75ac5c2e3fded19d0c14398bd2043e81c4ec8",
+			content:
+				"I'm 30 and looking to make a career change to web development. I have some basic HTML and CSS knowledge.  Wondering if a coding bootcamp would be a faster and more effective way to learn the necessary skills compared to self-learning online?",
+			reply: "As someone who has completed a coding bootcamp, I can attest to the effectiveness of a structured program like launchcode.com in accelerating your learning. The hands-on experience and guidance provided can significantly boost your understanding and skills in web development. While self-learning online is also a viable option, a bootcamp offers a more immersive and efficient approach to mastering coding languages. Good luck on your career change journey!",
+		},
+	],
 ];
 
 export const Hero = (boxProps: BoxProps) => {
@@ -59,147 +110,29 @@ export const Hero = (boxProps: BoxProps) => {
 				<div className={classes.marquee_grid}>
 					{/* first */}
 
-					<div className={classes.marquee}>
-						<div className={`${classes.marquee_track} ${classes.animate_marquee}`}>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-						</div>
-						<div
-							className={`${classes.marquee_track} ${classes.animate_marquee}`}
-							aria-hidden="true"
-						>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-						</div>
-					</div>
+					{replyCards.map((cards, idx) => {
+						const isMiddle = idx === 1;
 
-					{/* second */}
-
-					<div className={classes.marquee}>
-						<div className={`${classes.marquee_track} ${classes.animate_marquee_fast}`}>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-						</div>
-						<div
-							className={`${classes.marquee_track} ${classes.animate_marquee_fast}`}
-							aria-hidden="true"
-						>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-						</div>
-					</div>
-
-					{/* third */}
-
-					<div className={classes.marquee}>
-						<div className={`${classes.marquee_track} ${classes.animate_marquee}`}>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-						</div>
-						<div
-							className={`${classes.marquee_track} ${classes.animate_marquee}`}
-							aria-hidden="true"
-						>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-							<ReplyCard
-								username="John Doe"
-								avatar="https://www.redditstatic.com/shreddit/assets/snoovatar-back-64x64px.png"
-								content="Hello! I'm looking for a lawn mowing service"
-								reply="I can help you with that! I have a lawn mower and I'm available this weekend"
-							/>
-						</div>
-					</div>
+						return (
+							<div className={classes.marquee}>
+								<div
+									className={`${classes.marquee_track} ${isMiddle ? classes.animate_marquee_fast : classes.animate_marquee}`}
+								>
+									{cards.map((card, idx) => (
+										<ReplyCard {...card} key={idx} />
+									))}
+								</div>
+								<div
+									className={`${classes.marquee_track} ${isMiddle ? classes.animate_marquee_fast : classes.animate_marquee}`}
+									aria-hidden="true"
+								>
+									{cards.map((card, idx) => (
+										<ReplyCard {...card} key={idx} />
+									))}
+								</div>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</Box>
