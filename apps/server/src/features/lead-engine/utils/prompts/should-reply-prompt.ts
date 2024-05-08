e49746e1382a21963e81db7716f1db1610a25cea
Wes Lowe <wesley.lowe@yahoo.com>
2024-05-08 15:20:10 -0500

    updated prompts to introduce product first

--- apps/server/src/features/lead-engine/utils/prompts/should-reply-prompt.ts
+++ apps/server/src/features/lead-engine/utils/prompts/should-reply-prompt.ts
@@ -8,12 +8,12 @@ export type ShouldReplyPromptData = {
 export const shouldReplyPrompt = ({ lead, project }: ShouldReplyPromptData) => {
 	return {
 		system: "Given a text-based social media post and information about a product, determine if the post represents a viable marketing opportunity. Only output a boolean representing if the media post could be effectively replied to with a mention of the product.",
-		user: `Social media post: 
+		user: `Product name: ${project.name}
+Product description: ${project.description}
+
+Social media post: 
 \`\`\`
 ${lead.title ? `${lead.title}. ` : ""}${lead.content}
-\`\`\`
-
-Product name: ${project.name}
-Product description: ${project.description}`,
+\`\`\``,
 	};
 };
